1dfbb3bc1048f7f18443e158e6e4f9503dae51e9
Nikita Endaltsev <ne@login.ru>
2018-11-25 03:46:58 +0200

    update recipe representation

--- recipe/service/diet.py
+++ recipe/service/diet.py
@@ -19,6 +19,8 @@ def full_default_category_map(category_keys):
 
 def create_diet(diet_category_mapper, categories_map, recipes_map):
 
+    recipes_short = {}
+
     diet_suggestions = {
         k: {
             kk: [] for kk, vv in v.items()
@@ -57,21 +59,42 @@ def create_diet(diet_category_mapper, categories_map, recipes_map):
 
                 if approx_low <= kcal_portion <= approx_high:
 
-                    # custom_ingridient = []
-                    # ingridients = recipes_map[recipe_id].get('Ingredients')
-                    # ing = ingridients[0]
-                    # sub_ingridients = ing.get('SubSectionIngredients')
-                    # for sub_ing in sub_ingridients:
-                    #     for sub_sub_ing in sub_ing:
-                    #         try:
-                    #             custom_ingridient.append(sub_sub_ing)
-                    #         except:
-                    #             continue
-                    # custom_ingridient = list(custom_ingridient)
-                    # recipes_map[recipe_id]['products'] = get_products_list(custom_ingridient)
+                    custom_ingridient = []
+                    ingridients = recipes_map[recipe_id].get('Ingredients')
+                    ing = ingridients[0]
+                    sub_ingridients = ing.get('SubSectionIngredients')
+                    for sub_ing in sub_ingridients:
+                        for sub_sub_ing in sub_ing:
+                            try:
+                                custom_ingridient.append(sub_sub_ing)
+                            except:
+                                continue
+                    custom_ingridient = list(custom_ingridient)
+
+                    e = recipes_map[recipe_id]
+                    energy = e.get('EnergyAmounts')
+
+                    new_recipe = {
+                        'name': e.get('Name'),
+                        'portions': e.get('Portions').get('Amount'),
+                        'preparation_time': e.get('PreparationTime').get('Description'),
+                        'category': ''.join([c.get('MainName') for c in e.get('Categories')]),
+                        'img_url': [ee.get('Normal') for ee in e.get('PictureUrls')],
+                        'kcal_portion': float(kcal_portion),
+                        'kj_portion': float(energy.get('KJPerPortion')),
+                        'fat_portion': float(energy.get('FatPerPortion')),
+                        'protein_portion': float(energy.get('ProteinPerPortion')),
+                        'carbohydrate_portion': float(energy.get('CarbohydratePerPortion')),
+                        'instruction': e.get('Instructions'),
+                        'description': e.get('Description'),
+                        'k_url': e.get('Url'),
+                        'ingredients': ', '.join([ee.get('Name') for ee in custom_ingridient])
+                    }
+
+                    # new_recipe['products'] = get_products_list(custom_ingridient)
 
                     diet_suggestions[diet_level][meal_level].append(
-                        recipes_map[recipe_id]
+                        new_recipe
                     )
 
                     meals.add(recipe_id)
@@ -81,3 +104,54 @@ def create_diet(diet_category_mapper, categories_map, recipes_map):
                         break
 
     return diet_suggestions
+#         energy = e.get('EnergyAmounts')
+#
+#         if not isinstance(energy, dict):
+#             continue
+#
+#         ingredients = e.get('Ingredients')
+#         ingredients_internal_ids = []
+#         for ing in ingredients:
+#             sub_ings = ing.get('SubSectionIngredients')
+#
+#             for sub_ing_l in sub_ings:
+#                 for sub_ing in sub_ing_l:
+#
+#                     ing_type_id = sub_ing.get('IngredientType')
+#
+#                     if ing_type_id:
+#                         ingredient = Ingredient.objects.create(
+#                             id=ing_type_id,
+#                             name=sub_ing.get('Name')
+#                         )
+#                         ingredient.save()
+#                         ingredients_internal_ids.append(ing_type_id)
+#
+#         try:
+#             recipe = Recipe.objects.create(
+#                 id=e.get('Id'),
+#                 name=e.get('Name'),
+#                 portions=e.get('Portions').get('Amount'),
+#                 preparation_time=e.get('PreparationTime').get('Description'),
+#                 category=','.join(
+#                     [c.get('MainName') for c in e.get('Categories')]
+#                 ),
+#                 img_url=e.get('PictureUrls').get('Normal'),
+#                 kcal_portion=float(energy.get('KcalPerPortion')),
+#                 kj_portion=float(energy.get('KJPerPortion')),
+#                 fat_portion=float(energy.get('FatPerPortion')),
+#                 protein_portion=float(energy.get('ProteinPerPortion')),
+#                 carbohydrate_portion=float(
+#                     energy.get('CarbohydratePerPortion')
+#                 ),
+#
+#                 instruction=e.get('Instructions'),
+#                 description=e.get('Description'),
+#                 k_url=e.get('Url'),
+#                 ingredients=list(set(ingredients_internal_ids))
+#             )
+#
+#             recipe.save()
+#
+#         except Exception as e:
+#             print('... {}'.format(e))
